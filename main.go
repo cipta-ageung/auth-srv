e2003b0bff32a9f41bf5f40bc40ed84d09f53f82
Cipta Ageung Mahdiar <ciptaageung@Ciptas-MacBook-Pro.local>
2020-08-11 00:03:43 +0700

    add postgresql

--- main.go
+++ main.go
@@ -4,10 +4,11 @@ import (
 	"log"
 
 	"github.com/cipta-ageung/auth-srv/db"
-	"github.com/cipta-ageung/auth-srv/db/mysql"
+	"github.com/cipta-ageung/auth-srv/db/postgresql"
 	"github.com/cipta-ageung/auth-srv/handler"
 	account "github.com/cipta-ageung/auth-srv/proto/account"
 	oauth2 "github.com/cipta-ageung/auth-srv/proto/oauth2"
+	_ "github.com/lib/pq"
 	"github.com/micro/cli/v2"
 	"github.com/micro/go-micro/v2"
 )
@@ -17,16 +18,16 @@ func main() {
 		micro.Name("go.micro.srv.auth"),
 		micro.Flags(
 			&cli.StringFlag{
-				Name:   "database_url",
+				Name: "database_url",
 				//EnvVar: "DATABASE_URL",
-				Value: "root@tcp(127.0.0.1:3306)/auth",
-				Usage:  "The database URL e.g root@tcp(127.0.0.1:3306)/auth",
+				Value: "superman:agi12345@tcp(127.0.0.1:5432)/auth",
+				Usage: "The database URL e.g superman:agi12345@tcp(127.0.0.1:3306)/auth",
 			},
 		),
 
 		micro.Action(func(c *cli.Context) error {
 			if len(c.String("database_url")) > 0 {
-				mysql.Url = c.String("database_url")
+				postgresql.Url = c.String("database_url")
 			}
 
 			return nil
